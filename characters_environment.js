b9d0a2459e9f9b8fda3ac4455ab6b8cc13f17934
arnavkh21 <72095856+arnavkh21@users.noreply.github.com>
2021-12-06 16:04:18 +0300

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -52,8 +52,18 @@ var gameConfig={
 /*====================================
 =            Game Status             =
 ====================================*/
+noseX = "";
+noseY = "";
+GameStatus = "";
+
+function startGame(){
+  GameStatus = "start";
+  document.getElementById("status").innerHTML = "Game is Loading";
+}
 
 function game(){
+  console.log("noseX = " + noseX +", noseY = " + noseY);
+  
 
   instializeInDraw();
   moveEnvironment(mario);
